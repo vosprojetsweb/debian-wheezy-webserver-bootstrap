e5b62fd7671971e5880efc1757b637cc69a1c8fd
Christophe Borsenberger <christophe.borsenberger@vosprojetsweb.pro>
2013-05-23 17:17:21 +0200

    fix issue with tar

--- backup/remote-backup-server.sh
+++ backup/remote-backup-server.sh
@@ -7,7 +7,8 @@
 #		- répertoire /var/www
 #
 #	Script a installer sur la machine à sauvegarder, via la commande :
-#		wget -O /usr/local/bin/server-backup.sh https://raw.github.com/vosprojetsweb/debian-wheezy-webserver-bootstrap/master/backup/remote-backup-server.sh  
+#		sudo wget -O /usr/local/bin/server-backup.sh https://raw.github.com/vosprojetsweb/debian-wheezy-webserver-bootstrap/master/backup/remote-backup-server.sh
+#		sudo chmod 700 /usr/local/bin/server-backup.sh
 #
 
 set -e
@@ -21,14 +22,14 @@ BACKUP_DIR="/var/local/backup"
 # Backup MySQL
 MYSQL_BACKUP_FILE="${BACKUP_DIR}/mysql.gz"
 rm -f "${MYSQL_BACKUP_FILE}"
-mysqldump -u YOUR_USER -p YOUR_PASSWORD --opt --all-databases --single-transaction | gzip -9 > "${MYSQL_BACKUP_FILE}"
+mysqldump --opt --all-databases --events --single-transaction | gzip -9 > "${MYSQL_BACKUP_FILE}"
 
 # Backup /etc
 ETC_BACKUP_FILE="${BACKUP_DIR}/etc.tar.gz"
 rm -f "${ETC_BACKUP_FILE}"
-/bin/tar -czf "${ETC_BACKUP_FILE}" /etc 
+/bin/tar -czf "${ETC_BACKUP_FILE}" -C / etc/ 
 
 # Backup /var/www
 WWW_BACKUP_FILE="${BACKUP_DIR}/www.tar.gz"
 rm -f "${WWW_BACKUP_FILE}"
-/bin/tar -czf "${WWW_BACKUP_FILE}" --exclude "log" --exclude "tmp" /var/www 
+/bin/tar -czf "${WWW_BACKUP_FILE}" -C /var --exclude "log" --exclude "tmp" www/ 
